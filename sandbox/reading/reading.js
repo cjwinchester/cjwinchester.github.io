4551dd6b3d58309915a8125bf4c2f97fd2138e98
cjwinchester <cjwinchester@gmail.com>
2014-01-12 23:23:59 -0600

    Updated some stuff on reading page

--- sandbox/reading/reading.js
+++ sandbox/reading/reading.js
@@ -1,11 +1,12 @@
+    /*
+ jQuery Plugin: Table Filter - version 0.2.1
+
+ LICENSE: http://hail2u.mit-license.org/2009
+*/
+(function(a){a.fn.addTableFilter=function(d){var b=a.extend({},a.fn.addTableFilter.defaults,d),c,e;this.is("table")&&(this.attr("id")||this.attr({id:"t-"+Math.floor(99999999*Math.random())}),c=this.attr("id"),d=c+"-filtering",e=a("<label/>").attr({"for":d}).append(b.labelText),b=a('<input type="text" style="width:100%" id="search" placeholder="Filter ...">').attr({id:d,size:b.size}),a("<p/>").addClass("formTableFilter").append(e).append(b).insertBefore(this),a("#"+d).delayBind("keyup",function(){var b=a(this).val().toLowerCase().split(" ");a("#"+c+
+" tbody tr").each(function(){var d=a(this).html().toLowerCase().replace(/<.+?>/g,"").replace(/\s+/g," "),c=0;a.each(b,function(){if(0>d.indexOf(this))return c=1,!1});c?a(this).hide():a(this).show()})},300));return this};a.fn.addTableFilter.defaults=a.fn.delayBind=function(d,b,c,e){a.isFunction(b)&&(e=c,c=b,b=void 0);var g=this,f=null;return this.bind(d,b,function(b){clearTimeout(f);f=setTimeout(function(){c.apply(g,[a.extend({},b)])},e)})}})(jQuery);
+
 String.prototype.repeat = function(n){return new Array(isNaN(n) ? 1 : ++n).join(this);}
-
-var colorArray = ["CB181D,FCBBA1", "006D2C,99D8C9", "08519C,9ECAE1", "A63603,FDAE6B", "54278F,BCBDDC", "A50F15,FC9272"];
-
-var randomcolor = colorArray[Math.floor(Math.random() * colorArray.length)];
-
-var darkcol = randomcolor.substring(0,6);
-var lightcol = randomcolor.substring(7);
 
 function displayContent(json) {
             var table_content='';
@@ -20,17 +21,15 @@ function displayContent(json) {
             var emptystars = parseInt(json.feed.entry[i].gsx$empties.$t);
             var rating = "<i class='icon-star'></i>";
             var rating_rest = "<i class='icon-star-empty'></i>";
-            
-            var sugg = '<style>a {color:#' + darkcol + ';}a:visited {color:#' + lightcol + ';}</style><table><tr style="border-top:none; border-bottom:none;"><td colspan="3"><h2>What am I reading?</h2><div style="text-align:center; padding-top:10px; padding-bottom:10px; font-size:90%"><script type="text/javascript">var submitted=false;</script><iframe name="hidden_iframe" id="hidden_iframe" style="display:none;" onload="if(submitted) {window.location.reload()}"></iframe><form action="https://docs.google.com/forms/d/1eK0Dch0MNPG8lghTlTPZp-kXxjUJVmxEwLABcSyFTVg/formResponse" method="POST" target="hidden_iframe" onsubmit="submitted=true;">Also, what <em>should</em> I be reading?<br/><input type="text" style="width:120px;" name="entry.1059936569" value="" class="ss-q-short" id="entry_1059936569" dir="auto"><a class="fakebutton" type="submit" name="submit" style="vertical-align:7%; margin-left:5px; background:#' + darkcol + '">Submit &raquo;</a></form></div></td></tr>'
-            
+                        
             if (pages_done == pages_total) {fraction = "Finished"} else if (pages_done == 0) {fraction = "Not started"} else {fraction = ((pages_done / pages_total) * 100).toFixed(0) + '&#37; done"'};
             
             table_content += [
-            '<tr><td><a href="' + link + '" target="_blank"><em><strong>' + title + '</strong></em></a>, by ' + author + '</td><td>' + '<img class="imagechart" src="http://chart.googleapis.com/chart?chxs=0,00000000,11.5,0,_,00000000|1,00000000,11.5,0,t,00000000&chxt=x,y&chbh=a&chs=200x50&cht=bhs&chco=' + randomcolor + '&chds=0,' + pages_total + ',0,' + pages_total + '&chd=t:' + pages_done + '|' + (pages_total - pages_done) + '" title="' + fraction + '" alt="' + fraction + '"/>' + '</td><td style="width:70px;">' + rating.repeat(stars) + rating_rest.repeat(emptystars) + '</td></tr>'
+            '<tr><td><a href="' + link + '" target="_blank"><em><strong>' + title + '</strong></em></a>, by ' + author + '</td><td>' + '<img class="imagechart" src="http://chart.googleapis.com/chart?chxs=0,00000000,11.5,0,_,00000000|1,00000000,11.5,0,t,00000000&chxt=x,y&chbh=a&chs=200x50&cht=bhs&chco=0570b0,bdc9e1&chds=0,' + pages_total + ',0,' + pages_total + '&chd=t:' + pages_done + '|' + (pages_total - pages_done) + '" title="' + fraction + '" alt="' + fraction + '"/>' + '</td><td style="width:70px;">' + rating.repeat(stars) + rating_rest.repeat(emptystars) + '</td></tr>'
 ].join('');
-                        
+
 document.getElementById('whatreadingnow').innerHTML = 
-sugg + table_content + '</table>'
+'<table>' + table_content + '</table>'
 
 }
 
