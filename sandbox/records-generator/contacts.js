f457f279941066416bed490e9cd88304b788b0c0
cjwinchester <cjwinchester@gmail.com>
2014-01-14 13:38:23 -0600

    Create contacts.js

---
+++ sandbox/records-generator/contacts.js
@@ -0,0 +1,4 @@
+var govdata = [
+[ "Bellevue", "Dan Berlowitz", "City administrator", "555-555-5555", "address", "a@z.com" ],
+[ "Sarpy", "Fred Uhe", "Spokesman", "444-444-4444", "address", "a@z.com" ]
+]
