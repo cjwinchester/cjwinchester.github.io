8089fafba076e0d3690defad717a245786ae6c01
cjwinchester <cjwinchester@gmail.com>
2014-11-14 00:50:16 -0600

    wow added so much stuff

--- projects/js/projects.js
+++ projects/js/projects.js
@@ -4,7 +4,7 @@
         'type': 'app',
         'date': 'Nov. 4, 2014',
         'url': 'http://dataomaha.com/media/election2014/general/',
-        'desc': 'Nebraska general election returns, 2014. (<a href="http://blog.dataomaha.com/tarbelling-the-election/">Writeup</a>)'
+        'desc': 'Nebraska general election returns, 2014. (<a href="http://blog.dataomaha.com/tarbelling-the-election/">writeup</a>)'
     },
     {
         'hed': 'Prison scandal',
