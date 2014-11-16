08726cd7d866fc6eb07b345d60f273229abae769
cjwinchester <cjwinchester@gmail.com>
2014-11-16 03:29:01 -0600

    oh man what didn't i change here

--- js/projects.js
+++ js/projects.js
@@ -1,28 +1,28 @@
     var projects = [
    {
         'hed': 'Election results',
-        'type': 'app',
+        'type': 'web',
         'date': 'Nov. 4, 2014',
         'url': 'http://dataomaha.com/media/election2014/general/',
         'desc': 'Nebraska general election returns, 2014. (<a href="http://blog.dataomaha.com/tarbelling-the-election/">writeup</a>)'
     },
     {
         'hed': 'Prison scandal',
-        'type': 'app',
+        'type': 'web',
         'date': 'Sept. 28, 2014',
         'url': 'http://dataomaha.com/prison',
         'desc': 'A database of prisoners in Nebraska whose sentences were mistakenly cut short.'
     },
     {
         'hed': 'Public payroll',
-        'type': 'app',
+        'type': 'web',
         'date': 'March 2, 2014',
         'url': 'http://www.dataomaha.com/salaries',
         'desc': 'A database of thousands of public employee salaries in the Omaha metro area.'
     },
     {
         'hed': 'What am I reading?',
-        'type': 'misc',
+        'type': 'web',
         'date': 'Dec. 2, 2013',
         'url': '/reading',
         'desc': 'Books I\'m cracking.'
@@ -30,77 +30,77 @@
     {
         'hed': 'Military surplus shipments',
         'date': 'Sept. 20, 2014',
-        'type': 'app',
+        'type': 'web',
         'url': 'http://www.dataomaha.com/militarysurplus',
         'desc': 'A database of thousands of surplus items transferred to local law enforcement agencies, including assault rifles, armored vehicles and grenade launchers.'
     },
     {
         'hed': 'Bid problems',
         'date': 'May 20, 2014',
-        'type': 'art',
+        'type': 'print',
         'url': 'http://www.omaha.com/news/metro/with-douglas-co-director-under-scrutiny-state-halts-work-on/article_7bc74930-9131-5cdb-97da-0a8ed946f8dc.html?TNNoMobile',
         'desc': 'A county 911 director is investigated for his role in bidding a $10 million state contract.'
     }, 
     {
         'hed': 'Dutch World-Herald',
         'date': 'Jan. 6, 2014',
-        'type': 'misc',
+        'type': 'web',
         'url': 'http://www.twitter.com/dutchowh',
         'desc': 'A Twitter bot that retweets the Omaha World-Herald account &mdash; in Dutch! (<a href="https://github.com/cjwinchester/dutchowh">repo</a>)'
     },    
     {
         'hed': 'Minimum wage analysis',
         'date': 'Sept. 7, 2014',
-        'type': 'app',
+        'type': 'web',
         'url': 'http://www.dataomaha.com/minimumwage',
         'desc': 'A database and analysis of signatories to a statewide petition to raise the minimum wage.'
     },
     {
         'hed': 'Nuke plant costs',
         'date': 'May 7, 2014',
-        'type': 'app',
+        'type': 'web',
         'url': 'http://dataomaha.com/exelon',
         'desc': 'Tracking contractor invoices for the manager of a nuclear plant.'
     },
     {
         'hed': 'OPPD salaries',
         'date': 'March 2, 2014',
-        'type': 'art',
+        'type': 'print',
         'url': 'http://www.omaha.com/news/oppd-salaries-perks-keep-staff-from-straying/article_b3ae0ef5-e7cc-5cc9-8eb1-7b01dc09d493.html',
         'desc': 'Analyzing the pay scale at the local electrical utility.'
     },
     {
         'hed': 'The many names of David Ryder',
         'date': 'Jan. 12, 2014',
-        'type': 'misc',
+        'type': 'web',
         'url': 'http://cjwinchester.github.io/sandbox/ryder/',
         'desc': 'A tribute to the greatest space hero this side of Zapp Branagan.'
     },    
     {
         'hed': 'Medicare payments',
         'date': 'April 10, 2014',
-        'type': 'app',
+        'type': 'web',
         'url': 'http://dataomaha.com/medicare',
         'desc': 'A database of Medicare payments in Nebraska and Iowa.'
     },
     {
         'hed': 'Food in the newsroom',
         'date': 'Sept. 10, 2013',
-        'type': 'misc',
+        'type': 'web',
         'url': 'http://istherefoodinthenewsroom.us',
         'desc': 'Is there food in the World-Herald newsroom? This page maybe will tell you.'
     },
     {
         'hed': 'Analysis: 911 dispatch times',
         'date': 'May 5, 2013',
-        'type': 'art',
+        'type': 'print',
         'url': 'http://www.omaha.com/news/vital-time-lost-in-calls-to-sarpy-county-s-center/article_4773a77a-860f-540f-9c6b-766256873340.html?mode=jqm',
         'desc': 'Our analysis showed that Sarpy County dispatchers were taking too long to dispatch fire and medical calls.'
     },
     {
         'hed': 'OPPD energy options',
         'date': 'June 9, 2014',
-        'type': 'misc',
+        'type': 'web',
         'url': 'http://dataomaha.com/media/oppd-gen/',
         'desc': 'The local electrical utility moves away from coal.'
     }    
