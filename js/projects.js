139bd28db2bdc911c514c7f2745074d6a68f8a36
cjwinchester <cjwinchester@gmail.com>
2014-12-26 23:27:17 -0600

    oh just horsing around

--- js/projects.js
+++ js/projects.js
@@ -2,13 +2,13 @@ var projects = [
    {
         'hed': 'Election results',
         'type': 'web',
-        'date': 'Nov. 4, 2014',
+        'date': '2014-11-04',
         'url': 'http://dataomaha.com/media/election2014/general/',
         'desc': 'Parsing Nebraska\'s 2014 general election returns. (<a href="http://blog.dataomaha.com/tarbelling-the-election/">writeup</a>)'
     },
     {
         'hed': 'Bid problems',
-        'date': 'May 20, 2014',
+        'date': '2014-05-20',
         'type': 'print',
         'url': 'http://www.omaha.com/news/metro/with-douglas-co-director-under-scrutiny-state-halts-work-on/article_7bc74930-9131-5cdb-97da-0a8ed946f8dc.html?TNNoMobile',
         'desc': 'A county 911 director is investigated for his role in bidding a $10 million state contract.'
@@ -16,113 +16,120 @@ var projects = [
     {
         'hed': 'Prison scandal',
         'type': 'web',
-        'date': 'Sept. 28, 2014',
+        'date': '2014-09-28',
         'url': 'http://dataomaha.com/prison',
         'desc': 'A database of prisoners in Nebraska whose sentences were mistakenly cut short.'
     },
     {
         'hed': 'Public pay',
         'type': 'web',
-        'date': 'March 2, 2014',
+        'date': '2014-03-02',
         'url': 'http://www.dataomaha.com/salaries',
         'desc': 'A database of thousands of public employee salaries in the Omaha metro area.'
     },
     {
         'hed': 'What am I reading?',
         'type': 'web',
-        'date': 'Dec. 2, 2013',
+        'date': '2013-12-02',
         'url': '/reading',
         'desc': 'Books I\'m cracking.'
     },
     {
         'hed': 'OPPD pay analysis',
-        'date': 'March 2, 2014',
+        'date': '2014-03-02',
         'type': 'print',
         'url': 'http://www.omaha.com/news/oppd-salaries-perks-keep-staff-from-straying/article_b3ae0ef5-e7cc-5cc9-8eb1-7b01dc09d493.html',
         'desc': 'Taking a close look at the public utlity\'s pay scale.'
     },
     {
         'hed': 'Military surplus data',
-        'date': 'Sept. 20, 2014',
+        'date': '2014-09-20',
         'type': 'web',
         'url': 'http://www.dataomaha.com/militarysurplus',
         'desc': 'A database of surplus items transferred to local law enforcement agencies.'
     },
     {
         'hed': 'Dutch World-Herald',
-        'date': 'Jan. 6, 2014',
+        'date': '2014-01-06',
         'type': 'web',
         'url': 'http://www.twitter.com/dutchowh',
         'desc': 'A Twitter bot that retweets the Omaha World-Herald account &mdash; in Dutch! (<a href="https://github.com/cjwinchester/dutchowh">repo</a>)'
     },    
     {
         'hed': 'Minimum wage hike',
-        'date': 'Sept. 7, 2014',
+        'date': '2014-09-07',
         'type': 'web',
         'url': 'http://www.dataomaha.com/minimumwage',
         'desc': 'A database and analysis of a petition drive to raise the minimum wage.'
     },
     {
         'hed': 'Nuke plant costs',
-        'date': 'May 7, 2014',
+        'date': '2014-05-07',
         'type': 'web',
         'url': 'http://dataomaha.com/media/exelon',
         'desc': 'Tracking contractor invoices for the manager of a nuclear plant.'
     },
     {
         'hed': 'The many names of David Ryder',
-        'date': 'Jan. 12, 2014',
+        'date': '2014-01-12',
         'type': 'web',
         'url': '/z/ryder/',
         'desc': 'A tribute to the greatest space hero this side of Zapp Branagan.'
     },
         {
         'hed': 'Double-dipping',
-        'date': 'Oct. 6, 2013',
+        'date': '2013-10-06',
         'type': 'print',
         'url': 'http://www.omaha.com/news/more-than-retired-city-of-omaha-employees-return-to-their/article_277a27e3-9c58-5d5c-82f8-b1cc7b774760.html',
         'desc': 'We examined how retired city workers can return to their jobs while drawing a pension.'
     },
     {
         'hed': 'Medicare payments',
-        'date': 'April 10, 2014',
+        'date': '2014-04-10',
         'type': 'web',
         'url': 'http://dataomaha.com/medicare',
         'desc': 'A database of Medicare payments in Nebraska and Iowa.'
     },
     {
         'hed': 'Food in the newsroom',
-        'date': 'Sept. 10, 2013',
+        'date': '2013-09-10',
         'type': 'web',
         'url': 'http://istherefoodinthenewsroom.us',
         'desc': 'Is there food in the World-Herald newsroom?'
     },
     {
         'hed': 'Analysis: 911 dispatch times',
-        'date': 'May 5, 2013',
+        'date': '2013-05-05',
         'type': 'print',
         'url': 'http://www.omaha.com/news/vital-time-lost-in-calls-to-sarpy-county-s-center/article_4773a77a-860f-540f-9c6b-766256873340.html?mode=jqm',
         'desc': 'We found that dispatchers in one county were taking too long to dispatch fire and medical calls.'
     },
     {
         'hed': 'USA! USA! USA!',
-        'date': 'Nov. 19, 2013',
+        'date': '2014-11-19',
         'type': 'web',
         'url': '/z/usa',
         'desc': 'A running tally of where we\'ve lived and where we\'ve visited.'
     },
     {
         'hed': 'OPPD\'s energy options',
-        'date': 'June 9, 2014',
+        'date': '2014-06-09',
         'type': 'web',
         'url': 'http://dataomaha.com/media/oppd-gen/',
         'desc': 'OPPD moves away from coal.'
     },
     {
-        'hed': 'Jails, ERs new front line in battling mental illness',
-        'date': 'April 22, 2014',
+        'hed': 'Battling mental illness',
+        'date': '2014-04-22',
         'type': 'print',
         'url': 'http://m.omaha.com/eedition/sunrise/articles/jails-and-ers-new-front-line-in-battling-mental-illness/article_f4852cbe-3b18-5782-aa5a-0c7263d994e4.html?mode=jqm',
         'desc': 'Delving into the problems of an overburdened mental health system.'
-    }    
+    },
+    {
+        'hed': 'Happy Birthday!',
+        'date': '2014-12-26',
+        'type': 'web',
+        'url': 'http://www.codywinchester.com/z/birthday/',
+        'desc': 'Wish your loved one a happy birthday. (Hint: Pass his/her name as a URL hash.)'
+    }
 ]
