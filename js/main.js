4a4f283f0ebb1634af15122e55a9e235d093f316
cjwinchester <cjwinchester@gmail.com>
2015-03-09 01:34:19 -0500

    redid main page with underscore templates

--- js/main.js
+++ js/main.js
@@ -1,50 +1,37 @@
-var apmonths = [ "Jan.", "Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec." ]
+var apmonths = [ "Jan.", "Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec." ];
 
-    var proj = '';
-    for (i=0;i<projects.length;i++) {
-        var col_class = '';
-        var morekeys = [];
-        var d = new Date(projects[i].date);
-        var thisdate = apmonths[d.getMonth()] + " " + d.getUTCDate() + ", " + d.getFullYear();
-        if ( projects[i].type === "web" ) { col_class = '#4393c3' } else { col_class = '#de2d26' }
-        
-        if ( projects[i].collab ) { 
-            if ( projects[i].collab.length > 1 ) {
-            var collab = [];
-                for (q=0;q<projects[i].collab.length;q++) {
-                    collab.push('<a href="http://www.twitter.com/' + projects[i].collab[q].twitter + '" target="_blank">@' + projects[i].collab[q].twitter + '</a>');
-                    }
-                collab = ' With ' + collab.join(' and ') + '.';
-            }
-            else { var collab = ' With <a href="http://www.twitter.com/' + projects[i].collab[0].twitter + '" target="_blank">@' + projects[i].collab[0].twitter + '</a>' + '.'} 
-        } else { var collab = '' };
-
-       if ( projects[i].tools ) {
-            if ( projects[i].tools.constructor === Array ) {
-            var tools = [];
-                for (z=0;z<projects[i].tools.length;z++) {
-                tools.push('#' + projects[i].tools[z]);
-                morekeys.push(projects[i].tools[z].toLowerCase());
-                }
-                tools = '<p class="smallblock italic">' + tools.join('&emsp;') + '</p>';
-            }        
-            else { 
-            morekeys.push(projects[i].tools.toLowerCase());
-            var tools = '<p class="smallblock italic">#' + projects[i].tools + '</p>'} 
-      } else { var tools = '' }
-         
-        proj += [
-        '<div class="col-md-7 item searchable ' + projects[i].type + '" data-index="' + projects[i].hed.toLowerCase() + " " + morekeys.join(' ') + '" style="border-left:6px solid ' + col_class + ';"><p class="smallblock bold" style="margin-bottom:-15px;">' + thisdate + '</p><h3 class="bold" style="color:' + col_class + '"><a href="' + projects[i].url + '" target="_blank">' + projects[i].hed + '</a></h3><p>' + projects[i].desc + collab + '</p>' + tools + '</div>'
-        ].join('');
-    }
-
-    document.getElementById('projects').innerHTML = proj;
-    
-    $('#projects > div > p > a').each(function() {
-        var acol = $(this).parent().parent().css("border-left-color");
-        $(this).css('color', acol);        
+function apDate(dateobj) {
+    var d = new Date(dateobj), thisdate = apmonths[d.getMonth()] + " " + d.getUTCDate() + ", " + d.getFullYear();
+    return thisdate;
+  };
+  
+function collab(arr) {
+    var linkAdd = [];
+    $.each(arr, function(index, value) {
+        linkAdd.push('<a href="http://www.twitter.com/' + value + '">@' + value + "</a>")
     });
-        
+    return linkAdd.join(" and ");
+}
+
+function tools(arr) {
+    var formatted = [];
+    $.each(arr, function(index, value) {
+        formatted.push('#' + value);
+    });
+    return formatted.join('&emsp;');
+};
+
+_.templateSettings.variable = "banana";
+
+var template = _.template($( "script.template" ).html());            
+
+$('#projects').html(template( data ));
+
+$('#projects > div > p > a').each(function() {
+    var acol = $(this).parent().parent().css("border-left-color");
+    $(this).css('color', acol);        
+});
+
     $('#projects').fadeIn();
     
     $('.clicker').click(function() {
--- js/projects.js
+++ js/projects.js
@@ -1,21 +1,20 @@
-var projects = [
+var data = {
+    projects: [
    {
         'hed': 'Election results',
         'type': 'web',
         'date': '2014-11-04',
         'url': 'http://dataomaha.com/media/election2014/general/',
         'desc': 'Nebraska 2014 general election returns (<a href="http://blog.dataomaha.com/tarbelling-the-election/">writeup</a>).',
-        'collab': [ { 'name': 'Ben Vankat', 'twitter': 'bvankat' } ],
-        'tools': ['Tarbell', 'D3']
+        'collab': [ 'bvankat' ],
+        'tools': ['tarbell', 'd3']
     },
     {
         'hed': 'Bid problems',
         'date': '2014-05-20',
         'type': 'print',
         'url': 'http://www.omaha.com/news/metro/with-douglas-co-director-under-scrutiny-state-halts-work-on/article_7bc74930-9131-5cdb-97da-0a8ed946f8dc.html?TNNoMobile',
-        'desc': 'A county 911 director is investigated for his role in bidding a $10 million state contract.',
-        'collab': null,
-        'tools': null
+        'desc': 'A county 911 director is investigated for his role in bidding a $10 million state contract.'
     }, 
     {
         'hed': 'Prison scandal',
@@ -23,8 +22,8 @@ var projects = [
         'date': '2014-09-28',
         'url': 'http://dataomaha.com/prison',
         'desc': 'A database of prisoners in Nebraska whose sentences were mistakenly cut short.',
-        'collab': [ { 'name': 'Ben Vankat', 'twitter': 'bvankat' }, { 'name': 'Matt Wynn', 'twitter': 'mattwynn' } ],
-        'tools': 'Django'
+        'collab': [ 'bvankat', 'mattwynn' ],
+        'tools': ['django']
     },
     {
         'hed': 'Public pay',
@@ -32,8 +31,8 @@ var projects = [
         'date': '2014-03-02',
         'url': 'http://www.dataomaha.com/salaries',
         'desc': 'A database of thousands of public employee salaries in the Omaha metro area.',
-        'collab': [ { 'name': 'Ben Vankat', 'twitter': 'bvankat' }, { 'name': 'Matt Wynn', 'twitter': 'mattwynn' } ],
-        'tools': 'Django'
+        'collab': [ 'bvankat', 'mattwynn' ],
+        'tools': ['django']
     },
     {
         'hed': 'What am I reading?',
@@ -41,17 +40,14 @@ var projects = [
         'date': '2013-12-02',
         'url': '/reading',
         'desc': 'Books I\'m cracking.',
-        'collab': null,
-        'tools': 'Javascript'
+        'tools': ['javascript']
     },
     {
         'hed': 'OPPD pay analysis',
         'date': '2014-03-02',
         'type': 'print',
         'url': 'http://www.omaha.com/news/oppd-salaries-perks-keep-staff-from-straying/article_b3ae0ef5-e7cc-5cc9-8eb1-7b01dc09d493.html',
-        'desc': 'Taking a closer look at the local electrical utility\'s pay scale.',
-        'collab': null,
-        'tools': null
+        'desc': 'Taking a closer look at the local electrical utility\'s pay scale.'
     },
     {
         'hed': 'Military surplus data',
@@ -59,8 +55,8 @@ var projects = [
         'type': 'web',
         'url': 'http://www.dataomaha.com/militarysurplus',
         'desc': 'A database of surplus military items transferred to law enforcement agencies in Nebraska and Iowa.',
-        'collab': [ { 'name': 'Leah Becerra', 'twitter': 'leahbecerra' } ],
-        'tools': 'Django'
+        'collab': [ 'leahbecerra' ],
+        'tools': ['django']
     },
     {
         'hed': 'Dutch World-Herald',
@@ -68,8 +64,7 @@ var projects = [
         'type': 'web',
         'url': 'http://www.twitter.com/dutchowh',
         'desc': 'A bot that retweets the Omaha World-Herald account &mdash; in Dutch! (<a href="https://github.com/cjwinchester/dutchowh" target="_blank">repo</a>/<a href="http://blog.dataomaha.com/dit-is-een-blogbericht-ja" target="_blank">writeup</a>)',
-        'collab': null,
-        'tools': 'Python'
+        'tools': ['python']
     },    
     {
         'hed': 'Minimum wage hike',
@@ -77,8 +72,8 @@ var projects = [
         'type': 'web',
         'url': 'http://www.dataomaha.com/minimumwage',
         'desc': 'A database and analysis of a petition drive to raise the minimum wage.',
-        'collab': [ { 'name': 'Matt Wynn', 'twitter': 'mattwynn' } ],
-        'tools': 'Django'
+        'collab': [ 'mattwynn' ],
+        'tools': ['django']
     },
     {
         'hed': 'Nuke plant costs',
@@ -86,8 +81,7 @@ var projects = [
         'type': 'web',
         'url': 'http://dataomaha.com/media/exelon',
         'desc': 'Tracking contractor invoices for the manager of OPPD\'s nuclear plant.',
-        'collab': null,
-        'tools': 'Javascript'
+        'tools': ['javascript']
     },
     {
         'hed': 'Nebraska choroplethinator',
@@ -95,8 +89,7 @@ var projects = [
         'type': 'web',
         'url': 'https://github.com/cjwinchester/ne-county-choropleth',
         'desc': 'Shade a county map based on csv data.',
-        'collab': null,
-        'tools': ['Python', 'Inkscape']
+        'tools': ['python', 'inkscape']
     },
     {
         'hed': 'The many names of David Ryder',
@@ -104,8 +97,7 @@ var projects = [
         'type': 'web',
         'url': '/z/ryder/',
         'desc': 'A tribute to the greatest space hero this side of Zapp Branagan.',
-        'collab': null,
-        'tools': 'Javascript'
+        'tools': ['javascript']
     },
         {
         'hed': 'Double-dipping',
@@ -113,8 +105,7 @@ var projects = [
         'type': 'print',
         'url': 'http://www.omaha.com/news/more-than-retired-city-of-omaha-employees-return-to-their/article_277a27e3-9c58-5d5c-82f8-b1cc7b774760.html',
         'desc': 'We examined how retired city workers can return to their jobs while drawing a pension.',
-        'collab': [ { 'name': 'Matt Wynn', 'twitter': 'mattwynn' } ],
-        'tools': null
+        'collab': [ 'mattwynn' ]
     },
     {
         'hed': 'Medicare payments',
@@ -122,26 +113,16 @@ var projects = [
         'type': 'web',
         'url': 'http://dataomaha.com/medicare',
         'desc': 'A database of Medicare payments in Nebraska and Iowa.',
-        'collab': [ { 'name': 'Ben Vankat', 'twitter': 'bvankat' } ],
-        'tools': 'Django'
+        'collab': [ 'bvankat' ],
+        'tools': ['django']
     },
-/*    {
-        'hed': 'Food in the newsroom',
-        'date': '2013-09-10',
-        'type': 'web',
-        'url': 'http://istherefoodinthenewsroom.us',
-        'desc': 'Is there food in the World-Herald newsroom?',
-        'collab': null,
-        'tools': 'Javascript'
-    },*/
     {
         'hed': 'Analysis: 911 dispatch times',
         'date': '2013-05-05',
         'type': 'print',
         'url': 'http://www.omaha.com/news/vital-time-lost-in-calls-to-sarpy-county-s-center/article_4773a77a-860f-540f-9c6b-766256873340.html?mode=jqm',
         'desc': 'Dispatch times for fire and medical calls lagged in one county we examined.',
-        'collab': [ { 'name': 'Roseann Moring', 'twitter': 'roseannmoring' } ],
-        'tools': null
+        'collab': [ 'roseannmoring' ],
     },
     {
         'hed': 'USA! USA! USA!',
@@ -149,26 +130,15 @@ var projects = [
         'type': 'web',
         'url': '/z/usa',
         'desc': 'A running tally of where we\'ve lived and visited.',
-        'collab': null,
-        'tools': ['Inkscape', 'Javascript']
+        'tools': ['inkscape', 'javascript']
     },
-/*    {
-        'hed': 'OPPD\'s energy options',
-        'date': '2014-06-09',
-        'type': 'web',
-        'url': 'http://dataomaha.com/media/oppd-gen/',
-        'desc': 'Omaha\'s electrical utility moves away from coal.',
-        'collab': null,
-        'tools': 'Javascript'
-    },*/
     {
         'hed': 'Battling mental illness',
         'date': '2014-04-22',
         'type': 'print',
         'url': 'http://m.omaha.com/eedition/sunrise/articles/jails-and-ers-new-front-line-in-battling-mental-illness/article_f4852cbe-3b18-5782-aa5a-0c7263d994e4.html?mode=jqm',
         'desc': 'Delving into the problems of an overburdened mental health system.',
-        'collab': [ { 'name': 'Roseann Moring', 'twitter': 'roseannmoring' } ],
-        'tools': null
+        'collab': [ 'roseannmoring' ]
     },
     {
         'hed': 'Happy Birthday!',
@@ -176,8 +146,7 @@ var projects = [
         'type': 'web',
         'url': 'http://www.codywinchester.com/birthday.html#Shooter%20McGavin',
         'desc': 'Use this page to wish your loved one a happy birthday. (Hint: Pass his/her name as a URL hash.)',
-        'collab': null,
-        'tools': 'Javascript'
+        'tools': ['javascript']
     },
     {
         'hed': 'Cody Ipsum',
@@ -185,7 +154,6 @@ var projects = [
         'type': 'web',
         'url': 'http://www.codywinchester.com/tools/cody-ipsum/',
         'desc': 'My dummy text generator.',
-        'collab': null,
-        'tools': 'Javascript'
+        'tools': ['javascript']
     }
-]
+]};
