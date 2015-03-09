495d123a1345ee4ea209d459aa861c8227beba09
cjwinchester <cjwinchester@gmail.com>
2015-03-09 03:16:09 -0500

    arrow keys on maps + more underscore

---
+++ z/usa/js/main.js
@@ -0,0 +1,45 @@
+var data = { names: [{name: 'cody'},{name: 'laurel'},{name:'julian'},{name: 'lucy'}]};
+
+_.templateSettings.variable = "maps";
+var template = _.template($( "script.template" ).html());
+$('#main').html(template( data ));
+
+
+function getIt() {
+    $('li').removeClass('selected');
+    $('div.map').hide();
+}
+
+$('li').click(function() {
+  $( 'li' ).removeClass( 'selected' );
+  $('div.map').hide();
+  $( this ).addClass( 'selected' );
+  var num = $( this ).index();
+  $('div.map').eq(num).show();
+});
+
+$( window ).keyup(function() {
+    var curr, next, previous;
+        $( 'li' ).each(function() {
+        if ( $(this).hasClass('selected') )
+        { curr = this; }
+    });    
+    if ( event.keyCode === 39 ) {
+        getIt();
+        if ( $(curr).index() + 1 > $('li').length -1 ) {
+            next = 0;
+            }
+        else { next = $(curr).index() + 1 }
+        $('li').eq(next).addClass('selected');
+        $('div.map').eq(next).show();
+    }
+    else if ( event.keyCode === 37 ) {
+        getIt();
+        if ( typeof $(curr).index() - 1 === "undefined" ) {
+            previous = $('li').length;
+            }
+        else { previous = $(curr).index() - 1 }
+        $('li').eq(previous).addClass('selected');
+        $('div.map').eq(previous).show();
+    }    
+});   
