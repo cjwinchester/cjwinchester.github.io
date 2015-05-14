85cef3e6ec64b1b70aacdbdf4702a9d95015b40a
cjwinchester <cjwinchester@gmail.com>
2015-05-13 23:30:00 -0500

    ...

--- reading/js/reading.js
+++ reading/js/reading.js
@@ -9,19 +9,19 @@ var eldudebros = setInterval(function() {
     var el = document.getElementById('ellipses'),
         pses = ellipses(el.innerHTML.length);
     el.innerHTML = pses;
-    console.log('derp');
 }, 200);
 
-window.onload = eldudebros;
-
-$.getJSON('js/books.json').success(function(data) {
-    var template = _.template(document.getElementById('template').innerHTML);            
-    document.getElementById('output').innerHTML = template(data);
-}).then(function() {
-    $('#loading').fadeOut('fast');
-    $('#output').fadeIn('fast');
-    clearInterval(eldudebros);
-}).fail(function() { alert("Sorry, something went wrong. Try reloading the page."); });
+$(document).ready(function() {
+    eldudebros;
+    $.getJSON('js/books.json').success(function(data) {
+        var template = _.template(document.getElementById('template').innerHTML);            
+        document.getElementById('output').innerHTML = template(data);
+    }).then(function() {
+        $('#loading').fadeOut('fast');
+        $('#output').fadeIn('fast');
+        clearInterval(eldudebros);
+    }).fail(function() { alert("Sorry, something went wrong. Try reloading the page."); });
+});
 
 function womp(full) {
     var f = _(full).times(function() { return "&starf;" }).join('')
