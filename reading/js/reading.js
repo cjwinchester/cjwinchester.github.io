8fc192292644c3db39caed66809397c4a0c411c0
cjwinchester <cjwinchester@gmail.com>
2015-05-13 23:21:29 -0500

    el dude bros!

--- reading/js/reading.js
+++ reading/js/reading.js
@@ -1,10 +1,25 @@
+function ellipses(d) {
+    return d === 1 ? '..' :
+           d === 2 ? '...' :
+           d === 3 ? '' :
+                     '.';
+}
+
+var eldudebros = setInterval(function() {
+    var el = document.getElementById('ellipses'),
+        pses = ellipses(el.innerHTML.length);
+    el.innerHTML = pses;
+}, 200);
+
 $(document).ready(function() {
+    eldudebros;
     $.getJSON('js/books.json').success(function(data) {
         var template = _.template(document.getElementById('template').innerHTML);            
         document.getElementById('output').innerHTML = template(data);
     }).then(function() {
         $('#loading').fadeOut('fast');
         $('#output').fadeIn('fast');
+        clearInterval(eldudebros);
     }).fail(function() { alert("Sorry, something went wrong. Try reloading the page."); });
 });
 
