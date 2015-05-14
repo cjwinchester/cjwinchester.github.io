d11210c05626c41ed3fdc706a4b83d2cec0b9b94
cjwinchester <cjwinchester@gmail.com>
2015-05-13 23:28:25 -0500

    how does literally anything work

--- reading/js/reading.js
+++ reading/js/reading.js
@@ -11,16 +11,16 @@ var eldudebros = setInterval(function() {
     el.innerHTML = pses;
 }, 200);
 
-$(document).ready(function() {
-    $.getJSON('js/books.json').success(function(data) {
-        var template = _.template(document.getElementById('template').innerHTML);            
-        document.getElementById('output').innerHTML = template(data);
-    }).then(function() {
-        $('#loading').fadeOut('fast');
-        $('#output').fadeIn('fast');
-        clearInterval(eldudebros);
-    }).fail(function() { alert("Sorry, something went wrong. Try reloading the page."); });
-});
+window.onload = eldudebros;
+
+$.getJSON('js/books.json').success(function(data) {
+    var template = _.template(document.getElementById('template').innerHTML);            
+    document.getElementById('output').innerHTML = template(data);
+}).then(function() {
+    $('#loading').fadeOut('fast');
+    $('#output').fadeIn('fast');
+    clearInterval(eldudebros);
+}).fail(function() { alert("Sorry, something went wrong. Try reloading the page."); });
 
 function womp(full) {
     var f = _(full).times(function() { return "&starf;" }).join('')
