a03671ef77b81d1ce8a821a21b8accdace813697
cjwinchester <cjwinchester@gmail.com>
2015-08-10 23:47:39 -0500

    wuuhhh

--- data/js/data.js
+++ data/js/data.js
@@ -20,3 +20,5 @@ var searchStyle = document.getElementById('search_style');
     }
         searchStyle.innerHTML = ".searchable:not([data-index*=\"" + this.value.toLowerCase() + "\"]) { display: none; }";
     });
+    
+/*{"name":"Meatball surgery","updated":"8/9/2015","desc":"One of my favorite tropes from the sitcom M*A*S*H is when the surgeons amble out of the operating room, stretch, and say something like, \"Man, I'm beat. Eighteen hours of meatball surgery.\" Gets me every time. Anyway, Netflix has all 11 seasons now, so I watched all of them and kept track of every time I heard the phrase \"meatball surgery.\"","format":"json","url":"meatball-surgery.json"},*/
