67905d3bba0e2b732e5ec3680b40f292e91d22d8
Cody Winchester <cjwinchester@gmail.com>
2015-12-16 22:30:42 -0600

    cleanup

--- tools/records-generator/contacts.js
+++
@@ -1,4 +0,0 @@
-var govdata = [
-[ "Bellevue", "Dan Berlowitz", "City administrator", "555-555-5555", "address", "a@z.com" ],
-[ "Sarpy", "Fred Uhe", "Spokesman", "444-444-4444", "address", "a@z.com" ]
-]
