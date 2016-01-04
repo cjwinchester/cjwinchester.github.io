43114763f0031f1f60ab9de2acd1be414355a926
Cody Winchester <cjwinchester@gmail.com>
2016-01-03 20:12:47 -0600

    makefile for county duder map, etc.

---
+++ z/counties/js/getcounties.sh
@@ -0,0 +1,2 @@
+make counties.json
+make clean
--- z/counties/js/main.js
+++ z/counties/js/main.js
@@ -18,69 +18,52 @@ var svg = d3.select("svg");
 var g = svg.append("g");
 
 queue()
-    .defer(d3.json, "js/us.json")
+    .defer(d3.json, "js/counties.json")
     .await(ready);
 
 function ready(error, us) {
-  if (error) throw error;
+    if (error) throw error;
+
+    var counties = topojson.feature(us, us.objects.counties).features;
+    var $c = $("#cn");
+      
+    var grouped_sorted = _.sortBy(_.groupBy(counties, function(z) { return z.properties['name'] }), function(q) { return q.length}).reverse();
   
-  svg.append("g")
-      .attr("class", "counties")      
-    .selectAll("path")
-      .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append("path")
-      .attr("d", path)
-      .attr("id", function(d) { return d.id });
-
-  svg.append("path")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("class", "states")
-      .attr("d", path);
-};
-
-var highlight = function(idlist) {
-    d3.selectAll("path")
+    _.each(grouped_sorted, function(d) {
+        $c.append("<tr class='searchable' data-index='" + d[0].properties['name'].toLowerCase() + "'><td class='click' data-attr='" + d[0].properties['name'] + "'>" + d[0].properties['name'] + " (" + d.length + ")</td></tr>");
+    });
+    
+    $('.click').click(function() {
+        var county = $(this).attr('data-attr');
+        d3.selectAll("path")
         .each(function(d) {
-            if (_.contains(idlist, d.id)) {
+            if (typeof d !== "undefined" && d.properties.name === county) {
                 d3.select(this)
                     .classed("highlight", true);
             } else {
                 d3.select(this)
                     .classed("highlight", false);
             };
-        });    
+        });
+    });
+  
+  svg.append("g")
+      .attr("class", "counties")      
+    .selectAll("path")
+      .data(counties)
+    .enter().append("path")
+      .attr("d", path);
+
 };
 
-$(document).ready(function() {
-    var $c = $("#cn");
-    $.getJSON('js/fips.json').success(function(z) {
-        var grouped_sorted = _.sortBy(_.groupBy(z, "county"), function(q) { return q.length}).reverse();
-        _.each(grouped_sorted, function(d) {
-            $c.append("<tr class='searchable' data-index='" + d[0].county.toLowerCase() + "'><td class='click' data-attr='" + d[0].county + "'>" + d[0].county + " (" + d.length + ")</td></tr>");
-        });
-    
-    $('.click').click(function() {
-        var county = $(this).attr('data-attr');
-        var matches = _.where(z, {'county': county});
-        var ls = [];
-        _.each(matches, function(x) {
-            ls.push(Number(x.scode + x.ccode));
-        });
-        highlight(ls);
-    });
-    
-    });
-
-	var searchStyle = document.getElementById('search_style');
-		document.getElementById('search').addEventListener('input', function() {
-			if (!this.value) {
-				searchStyle.innerHTML = "";
-			return;
-		}
-			searchStyle.innerHTML = ".searchable:not([data-index*=\"" + this.value.toLowerCase() + "\"]) { display: none; }";
-	});    
+var searchStyle = document.getElementById('search_style');
+document.getElementById('search').addEventListener('input', function() {
+	if (!this.value) {
+		searchStyle.innerHTML = "";
+	    return;
+    }
+    searchStyle.innerHTML = ".searchable:not([data-index*=\"" + this.value.toLowerCase() + "\"]) { display: none; }";
 });
-
 
 var updateLayout = _.debounce(function(e) {
     var targetWidth = $('#mapcontainer').width();
